779afd96ef7e8a7c22943f0219f571076079439b
sioterino <sofia.a2005@aluno.ifsc.edu.br>
2025-02-22 10:16:57 -0300

    dialog funcionando amem

--- js/dialog.js
+++ js/dialog.js
@@ -14,54 +14,56 @@ cards.forEach(card => {
 
         dialog.innerHTML = ''
         createCatCard(gatinhos[index], 'jpg', dialog)
+        eventListener(dialog)
 
         dialog.showModal()
     })
 })
 
 
-const openDialog = document.getElementById("openDialog")
-const closeDialog = dialog.querySelector('.botao-close')
+function eventListener(dialog) {
+    const closeDialog = dialog.querySelector('.botao-close')
 
-closeDialog.addEventListener("click", () => {
-    dialog.close()
-})
+    closeDialog.addEventListener("click", () => {
+        dialog.close()
+    })
 
-const pickImg = dialog.querySelectorAll(".img-pick")
-const carrosselDiv = dialog.querySelector(".carrossel")
-const leftArrowicon = dialog.querySelector(".pick-arrow-left")
-const rightArrowIcon = dialog.querySelector(".pick-arrow-right")
+    const pickImg = dialog.querySelectorAll(".img-pick")
+    const carrosselDiv = dialog.querySelector(".carrossel")
+    const leftArrowicon = dialog.querySelector(".pick-arrow-left")
+    const rightArrowIcon = dialog.querySelector(".pick-arrow-right")
 
-// // Scroll to the previous image with looping behavior
-leftArrowicon.addEventListener("click", () => {
-    const currentScroll = carrosselDiv.scrollLeft
-    const imageWidth = pickImg[0].offsetWidth + 1.2 * 16
-    const newScroll = currentScroll - imageWidth
+    // // Scroll to the previous image with looping behavior
+    leftArrowicon.addEventListener("click", () => {
+        const currentScroll = carrosselDiv.scrollLeft
+        const imageWidth = pickImg[0].offsetWidth + 1.2 * 16
+        const newScroll = currentScroll - imageWidth
 
-    if (newScroll < 0) {
-        carrosselDiv.scrollTo({ left: carrosselDiv.scrollWidth - carrosselDiv.clientWidth, behavior: "smooth" })
-    } else {
-        carrosselDiv.scrollTo({ left: newScroll, behavior: "smooth" })
-    }
-})
+        if (newScroll < 0) {
+            carrosselDiv.scrollTo({ left: carrosselDiv.scrollWidth - carrosselDiv.clientWidth, behavior: "smooth" })
+        } else {
+            carrosselDiv.scrollTo({ left: newScroll, behavior: "smooth" })
+        }
+    })
 
-// Scroll to the next image with looping behavior
-rightArrowIcon.addEventListener("click", () => {
-    const currentScroll = carrosselDiv.scrollLeft
-    const imageWidth = pickImg[0].offsetWidth + 1.2 * 16
-    const newScroll = currentScroll + imageWidth
+    // Scroll to the next image with looping behavior
+    rightArrowIcon.addEventListener("click", () => {
+        const currentScroll = carrosselDiv.scrollLeft
+        const imageWidth = pickImg[0].offsetWidth + 1.2 * 16
+        const newScroll = currentScroll + imageWidth
 
-    if (newScroll >= carrosselDiv.scrollWidth - carrosselDiv.clientWidth) {
-        carrosselDiv.scrollTo({ left: 0, behavior: "smooth" })
-    } else {
-        carrosselDiv.scrollTo({ left: newScroll, behavior: "smooth" })
-    }
-})
+        if (newScroll >= carrosselDiv.scrollWidth - carrosselDiv.clientWidth) {
+            carrosselDiv.scrollTo({ left: 0, behavior: "smooth" })
+        } else {
+            carrosselDiv.scrollTo({ left: newScroll, behavior: "smooth" })
+        }
+    })
 
-// animação do coração de favorito
-const fav = dialog.querySelector('.botao.botao-fav')
-fav.addEventListener('click', () => {
-    const heart = fav.querySelector('.icon')
-    heart.classList.toggle('hollow')
-    heart.classList.toggle('liked')
-})
+    // animação do coração de favorito
+    const fav = dialog.querySelector('.botao.botao-fav')
+    fav.addEventListener('click', () => {
+        const heart = fav.querySelector('.icon')
+        heart.classList.toggle('hollow')
+        heart.classList.toggle('liked')
+    })
+}
